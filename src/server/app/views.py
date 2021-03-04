7aa6cd45f3c88de7501da9b03b25ff4add88682e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-04 18:32:58 +0100

    Slightly more complicated Bokeh visualization running

--- src/server/app/views.py
+++ src/server/app/views.py
@@ -1,22 +1,57 @@
 from django.shortcuts import render
+
 from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, FactorRange
 from bokeh.embed import components
+from bokeh.palettes import Spectral6
+from bokeh.transform import factor_cmap
 
 
 def home(request):
 
-    x = [1, 2, 3, 4, 5]
-    y = [1, 2, 3, 4, 5]
+    fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+    years = ['2015', '2016', '2017']
+
+    data = {
+        'fruits': fruits,
+        '2015': [2, 1, 4, 3, 2, 4],
+        '2016': [5, 3, 3, 2, 4, 6],
+        '2017': [3, 2, 4, 4, 5, 3],
+    }
+
+    x_values = [(fruit, year) for fruit in fruits for year in years]
+    counts = sum(zip(data['2015'], data['2016'], data['2017']), ())
+
+    source = ColumnDataSource(data=dict(x=x_values, counts=counts))
 
     plot = figure(
-        title='Line graph',
-        x_axis_label='X',
-        y_axis_label='Y',
-        plot_width=400,
-        plot_height=400,
+        x_range=FactorRange(*x_values),
+        plot_height=250,
+        title='Fruit counts per year',
+        toolbar_location=None,
+        tools='',
     )
 
-    plot.line(x, y, line_width=2)
+    plot.vbar(
+        x='x',
+        top='counts',
+        width=0.9,
+        source=source,
+        line_color='white',
+        fill_color=factor_cmap(
+            'x',
+            palette=Spectral6,
+            factors=years,
+            start=1,
+            end=2
+        )
+    )
+
+    plot.y_range.start = 0
+    plot.x_range.range_padding = 0.1
+    plot.xaxis.major_label_orientation = 1
+    plot.xgrid.grid_line_color = None
 
     script, div = components(plot)
+
     return render(request, 'base.html', {'script': script, 'div': div})
