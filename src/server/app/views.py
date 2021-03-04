0f5dea69ebf1217fa4ed60f49fdb23f4e2f37520
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-04 18:22:25 +0100

    Simple Bokeh line graph running in Django

--- src/server/app/views.py
+++ src/server/app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 
 def home(request):
-    return render(request, 'base.html', {})
+
+    x = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+
+    plot = figure(
+        title='Line graph',
+        x_axis_label='X',
+        y_axis_label='Y',
+        plot_width=400,
+        plot_height=400,
+    )
+
+    plot.line(x, y, line_width=2)
+
+    script, div = components(plot)
+    return render(request, 'base.html', {'script': script, 'div': div})
