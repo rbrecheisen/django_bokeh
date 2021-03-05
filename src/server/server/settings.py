8a2863056e05e58cc19f83340e98dbe798cd6102
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-05 07:28:31 +0100

    Saving work

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = open(os.path.join(os.environ['HOME'], 'djangosecretkey.txt', 'r')).readline().strip()
+SECRET_KEY = open(os.path.join(os.environ['HOME'], 'djangosecretkey.txt'), 'r').readline().strip()
 
 DEBUG = True
 
