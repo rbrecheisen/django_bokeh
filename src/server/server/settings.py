02b2494a1732c62caa454f74e9973dcb99bbfb25
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-07 07:14:18 +0100

    Login works with Bootstrap5 styling

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
